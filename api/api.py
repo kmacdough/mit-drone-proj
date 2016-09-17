c12f37ea781bc77e09d1f6fad8ecec226a373681
Kevin McDonough <kmcdonough@placester.com>
2016-09-17 19:20:16 -0400

    Added Parsel POST endpoint

--- api/api.py
+++ api/api.py
@@ -5,7 +5,7 @@ from flask import jsonify, request
 from uuid import uuid4 as uuid
 
 from app import app, logger, db
-from models import Place
+from models import Place, Parcel
 
 @app.route('/test', methods=['GET'])
 def test():
@@ -18,3 +18,33 @@ def test_retrieval():
     print(Place.get_by_id(db, id_).to_dict())
     return jsonify(status='success'), 200
 
+
+@app.route('/parcel', methods=['POST'])
+def new_parcel():
+    request_json = request.get_json()
+    length = request_json["length"]
+    width = request_json["width"]
+    height = request_json["height"]
+    weight = request_json["weight"]
+    origin_uuid = request_json["origin"]
+    destination_uuid = request_json["destination"]
+
+    id_ = str(uuid())
+    origin = Place.get_by_id(origin_uuid).geolocation
+    destination = Place.get_by_id(destination_uuid).geolocation
+    location = origin
+
+    parcel = Parcel(
+        id_=id_,
+        length=length,
+        width=width,
+        height=height,
+        weight=weight,
+        origin=origin,
+        destination=destination,
+        location=location
+    )
+    return {
+        "status": "success",
+        "data": id_
+    }
